c445930fb1eb267a101baad1f35d79c8313f5f4d
unknown <vcaciuc@wk-vcaciuc.cs.ua.edu>
2014-05-22 16:55:56 -0500

    new class (menu)

--- app/src/main/java/ua/edu/myapplication2/app/Splash.java
+++ app/src/main/java/ua/edu/myapplication2/app/Splash.java
@@ -2,24 +2,24 @@ package ua.edu.myapplication2.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 /**
  * Created by vcaciuc on 5/22/2014.
  */
 public class Splash extends Activity {
+    MediaPlayer ourSong;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        ourSong = MediaPlayer.create(Splash.this, R.raw.music );
+        ourSong.start();
         Thread timer = new Thread(){
             public void run(){
                 try{
-                    sleep(1000);
-
-
-
-
+                    sleep(10000);
                 }catch(InterruptedException e){
                     e.printStackTrace();
                 }finally{
@@ -29,5 +29,12 @@ public class Splash extends Activity {
             }
         };
         timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ourSong.release();
+        finish();
     }
 }
